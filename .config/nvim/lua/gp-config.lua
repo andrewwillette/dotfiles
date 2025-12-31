f36ee0891bb5d4b916de6a1928ba5e17b0dd8175
andrewwillette <willette.andrew1846@gmail.com>
2025-12-31 17:22:25 -0600

    updating gp.nvim config

--- .config/nvim/lua/gp-config.lua
+++ .config/nvim/lua/gp-config.lua
@@ -3,8 +3,8 @@ require("gp").setup({
   openai_api_base = "https://api.openai.com/v1",
 
   model = "gpt-4o-mini",
-  default_chat_agent = "ChatGPT4o_new",
+  default_chat_agent = "ChatGPT4o",
   chat_free_cursor = true,
   toggle_target = "split",
-  openai_resp_tools = { { type = "web_search" } },
+  openai_tools = { { type = "web_search" } },
 })
