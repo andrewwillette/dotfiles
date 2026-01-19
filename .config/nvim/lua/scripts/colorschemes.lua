6e87a3fdb8e7c4f635d5a072277e5617c49d1c39
andrewwillette <willette.andrew1846@gmail.com>
2026-01-19 08:11:46 -0600

    removing uninstalled colorschemes (everything)

--- .config/nvim/lua/scripts/colorschemes.lua
+++ .config/nvim/lua/scripts/colorschemes.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 local verify_plugin = require("scripts").verify_nvim_plugin
-local verify_vim_cmd = require("scripts").verify_vim_command
 
 ---@param scheme string Lualine theme name
 local function set_lualine(scheme)
@@ -10,219 +9,18 @@ local function set_lualine(scheme)
   llc.set_theme(scheme)
 end
 
-local function monokaipro()
-  require("monokai-pro").setup({
-    transparent_background = false,
-    terminal_colors = true,
-    devicons = true, -- highlight the icons of `nvim-web-devicons`
-    italic_comments = false,
-    filter = "pro",  -- classic | octagon | pro | machine | ristretto | spectrum
-    -- Enable this will disable filter option
-    day_night = {
-      enable = false,            -- turn off by default
-      day_filter = "pro",        -- classic | octagon | pro | machine | ristretto | spectrum
-      night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-    },
-    inc_search = "background",   -- underline | background
-    background_clear = {
-      -- "float_win",
-      "toggleterm",
-      "telescope",
-      "which-key",
-      "renamer"
-    }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree"
-    plugins = {
-      bufferline = {
-        underline_selected = false,
-        underline_visible = false,
-      },
-      indent_blankline = {
-        context_highlight = "default", -- default | pro
-        context_start_underline = false,
-      },
-    },
-    override = function(_) end,
-  })
-  vim.cmd [[colorscheme monokai-pro]]
-  set_lualine("monokai-pro")
-end
-
-local function onedark()
-  require('onedark').setup {
-    -- Main options --
-    style = 'dark',               -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = false,          -- Show/hide background
-    term_colors = true,           -- Change terminal color as per the selected theme style
-    ending_tildes = false,        -- Show the end-of-buffer tildes. By default they are hidden
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-    -- toggle theme style ---
-    toggle_style_key = nil,                                                              -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
-
-    -- Change code style ---
-    -- Options are italic, bold, underline, none
-    -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
-    code_style = {
-      comments = 'none',
-      keywords = 'none',
-      functions = 'none',
-      strings = 'none',
-      variables = 'none'
-    },
-
-    -- Lualine options --
-    lualine = {
-      transparent = false, -- lualine center bar transparency
-    },
-
-    -- Custom Highlights --
-    colors = {},     -- Override default colors
-    highlights = {}, -- Override highlight groups
-
-    -- Plugins Config --
-    diagnostics = {
-      darker = true,     -- darker colors for diagnostic
-      undercurl = true,  -- use undercurl instead of underline for diagnostics
-      background = true, -- use background color for virtual text
-    },
-  }
-  require('onedark').load()
-end
-
-local function moonfly()
-  vim.g.moonflyItalics = false
-  vim.cmd [[colorscheme moonfly]]
-end
-
-local function tundra()
-  local ok, nvimtundra = verify_plugin("nvim-tundra")
-  if not ok then return end
-  nvimtundra.setup({
-    transparent_background = false,
-    editor = {
-      search = {},
-      substitute = {},
-    },
-    syntax = {
-      booleans = { bold = true, italic = false },
-      comments = { bold = true, italic = false },
-      conditionals = {},
-      constants = { bold = true },
-      functions = {},
-      keywords = {},
-      loops = {},
-      numbers = { bold = true },
-      operators = { bold = true },
-      punctuation = {},
-      strings = {},
-      types = { italic = false },
-    },
-    diagnostics = {
-      errors = {},
-      warnings = {},
-      information = {},
-      hints = {},
-    },
-    plugins = {
-      lsp = true,
-      treesitter = true,
-      cmp = true,
-      context = true,
-      dbui = true,
-      gitsigns = true,
-      telescope = false,
-    },
-    overwrite = {
-      colors = {},
-      highlights = {},
-    },
-  })
-  vim.opt.background = 'dark'
-  vim.cmd('colorscheme tundra')
-  set_lualine("codedark")
-end
-
-local function kanagawa()
-  local ok, kanag = verify_plugin("kanagawa")
-  if not ok then return end
-  kanag.setup({
-    compile = false,  -- enable compiling the colorscheme
-    undercurl = true, -- enable undercurls
-    commentStyle = { italic = false },
-    functionStyle = {},
-    keywordStyle = { italic = false },
-    statementStyle = { bold = false },
-    typeStyle = {},
-    transparent = false,   -- do not set background color
-    dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true, -- define vim.g.terminal_color_{0,17}
-    colors = {             -- add/modify theme and palette colors
-      palette = {},
-      theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-    },
-    overrides = function(_) -- add/modify highlights
-      return {}
-    end,
-    theme = "wave",  -- Load "wave" theme when 'background' option is not set
-    background = {   -- map the value of 'background' option to a theme
-      dark = "wave", -- try "dragon" !
-      light = "lotus"
-    },
-  })
-  -- setup must be called before loading
-  vim.cmd("colorscheme kanagawa")
-  set_lualine("nightfly")
-end
-
-local function kanagawabones()
-  vim.o.background = "dark"
-  vim.g.seoulbones = { italic_comments = false }
-  if not verify_vim_cmd("colorscheme kanagawabones") then return end
-  set_lualine("codedark")
-end
-
-local function seoulbonesdark()
-  vim.o.background = "dark"
-  vim.g.seoulbones = { italic_comments = false }
-  if not verify_vim_cmd("colorscheme seoulbones") then return end
-  set_lualine("seoul256")
-end
-
-local function seoulboneslight()
-  vim.o.background = "light"
-  vim.g.seoulbones = { italic_comments = false }
-  if not verify_vim_cmd("colorscheme seoulbones") then return end
-  set_lualine("seoul256")
-end
-
-local function greenisgood()
-  vim.o.background = "dark"
-  if not verify_vim_cmd("colorscheme greenisgood") then return end
-  set_lualine("codedark")
-end
-
-local function monochrome()
-  vim.o.background = "dark"
-  if not verify_vim_cmd("colorscheme monochrome") then return end
-  set_lualine("codedark")
-end
-
-local gruvboxmaterial = function()
-  vim.opt.termguicolors = true
-  vim.o.background = "dark"
-  vim.g.gruvbox_material_disable_italic_comment = 0
-  if not verify_vim_cmd("colorscheme gruvbox-material") then return end
-  set_lualine("gruvbox_dark")
-end
-
 local gruvboxlight = function()
   vim.o.background = "light"
   require("gruvbox").setup({
     undercurl = false,
     underline = false,
     bold = false,
-    italic = false,
+    italic = {
+      strings = false,
+      comments = false,
+      operators = false,
+      folds = false,
+    },
     strikethrough = false,
     invert_selection = false,
     invert_signs = false,
@@ -270,109 +68,9 @@ local gruvboxdark = function()
   set_lualine("gruvbox_dark")
 end
 
-local function rosepine(bg)
-  local ok, rosepineplugin = verify_plugin("rose-pine")
-  if not ok then return end
-  rosepineplugin.setup({
-    --- @usage 'main' | 'moon'
-    dark_variant = 'moon',
-    bold_vert_split = false,
-    dim_nc_background = false,
-    disable_background = false,
-    disable_float_background = false,
-    disable_italics = true,
-
-    --- @usage string hex value or named color from rosepinetheme.com/palette
-    groups = {
-      background = 'base',
-      panel = 'surface',
-      border = 'highlight_med',
-      comment = 'muted',
-      link = 'iris',
-      punctuation = 'subtle',
-
-      error = 'love',
-      hint = 'iris',
-      info = 'foam',
-      warn = 'gold',
-
-      headings = {
-        h1 = 'iris',
-        h2 = 'foam',
-        h3 = 'rose',
-        h4 = 'gold',
-        h5 = 'pine',
-        h6 = 'foam',
-      }
-      -- or set all headings at once
-      -- headings = 'subtle'
-    },
-
-    -- Change specific vim highlight groups
-    highlight_groups = {
-      ColorColumn = { bg = 'rose' }
-    }
-  })
-  vim.o.background = bg
-  vim.cmd("colorscheme rose-pine")
-end
-
-local rosepinelight = function()
-  rosepine "light"
-  set_lualine("ayu_light")
-end
-
-local rosepinedark = function()
-  rosepine "dark"
-  set_lualine("ayu_dark")
-end
-
-local duskfoxdark = function()
-  vim.opt.termguicolors = true
-  vim.o.background = "dark"
-  if not verify_vim_cmd("colorscheme duskfox") then return end
-  set_lualine("palenight")
-end
-
-local nightfoxdark = function()
-  vim.opt.termguicolors = true
-  vim.o.background = "dark"
-  vim.cmd("colorscheme nightfox")
-  set_lualine("palenight")
-end
-
-local dayfoxlight = function()
-  vim.opt.termguicolors = true
-  vim.o.background = "light"
-  if not verify_vim_cmd("colorscheme dayfox") then return end
-  set_lualine("ayu_light")
-end
-
-local everforest = function()
-  vim.o.background = "dark"
-  vim.cmd("colorscheme everforest")
-end
-
 local colorscheme_table = {
   ["gruvbox dark"] = gruvboxdark,
   ["gruvbox light"] = gruvboxlight,
-  ["gruvbox material"] = gruvboxmaterial,
-  ["rosepine light"] = rosepinelight,
-  ["rosepine dark"] = rosepinedark,
-  ["duskfox dark"] = duskfoxdark,
-  ["dayfox light"] = dayfoxlight,
-  ["nightfox dark"] = nightfoxdark,
-  ["greenisgood dark"] = greenisgood,
-  ["monochrome dark"] = monochrome,
-  ["seoulbones dark"] = seoulbonesdark,
-  ["seoulbones light"] = seoulboneslight,
-  ["kanagawabones"] = kanagawabones,
-  ["kanagawa"] = kanagawa,
-  ["tundra"] = tundra,
-  ["moonfly"] = moonfly,
-  ["onedark"] = onedark,
-  ["monokaipro"] = monokaipro,
-  ["everforest"] = everforest,
 }
 
 local colorscheme_select = {}
